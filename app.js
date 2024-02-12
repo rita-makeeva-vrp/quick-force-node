7ae76ab374f211c215281bb6ed54253a25570ab3
Rita <rita.makeeva@vrpconsulting.com>
2024-02-12 16:47:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,20 @@ hbs.registerHelper('get', function(field) {
   return this.get(field);
 });
 
+app.post("/new_contact", function(req, res) { 
+  var notification = req.body["soapenv:envelope"]["soapenv:body"][0]["notifications"][0]; 
+  var sessionId = notification["sessionid"][0]; 
+  var data = {};
+  if (notification["notification"] !== undefined) { 
+    var sobject = notification["notification"][0]["sobject"][0]; 
+    Object.keys(sobject).forEach(function(key) { 
+      if (key.indexOf("sf:") == 0) { 
+        var newKey = key.substr(3); data[newKey] = sobject[key][0]; } 
+    }); // do something #awesome with the data and sessionId 
+  } 
+  res.status(201).end(); 
+});
+
 app.get('/', function(req, res) {
   if (isSetup()) {
     var org = nforce.createConnection({
